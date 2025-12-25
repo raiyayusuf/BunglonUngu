2036d5e870a7812b81433b18e6174a6feb36ca76
Raiya Yusuf Priatmojo <raiyayusuf123@gmail.com>
2025-12-25 18:50:57 +0700

    uppp

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -3,13 +3,7 @@ export function renderNavbar() {
 
   navbar.innerHTML = /*html*/ `
     <div class="nav-container">
-      <a href="#home" class="nav-logo">Bloom Florist</a>
-      <ul class="nav-items">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#products">Products</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#cart">Cart</a></li>
-      </ul>
+      <!-- Brand / Logo -->
     </div>
   `;
 }
